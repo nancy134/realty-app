650d0a8dbfe93f21c5d3db773cd5df5101558bb1
Nancy Piedra <nancy_piedra@yahoo.com>
2020-08-18 14:16:03 +0000

    ACL-90 Pagination

--- src/components/ListingPagination.js
+++ src/components/ListingPagination.js
@@ -10,9 +10,40 @@ class ListingPagination extends React.Component {
         
     }
     onClick(goToPage){
-        console.log("onClick: goToPage: "+goToPage);
         this.props.onNewPage(goToPage);
     }
+
+    pagination(c, m) {
+        var current = c,
+            last = m,
+            delta = 2,
+            left = current - delta,
+            right = current + delta + 1,
+            range = [],
+            rangeWithDots = [],
+            l;
+
+        for (let i = 1; i <= last; i++) {
+            if (i === 1 || i === last || (i >= left && i < right)) {
+                range.push(i);
+            }
+        }
+
+        for (let i of range) {
+            if (l) {
+                if (i - l === 2) {
+                    rangeWithDots.push(l + 1);
+                } else if (i - l !== 1) {
+                    rangeWithDots.push('...');
+                }
+            }
+            rangeWithDots.push(i);
+            l = i;
+        }
+
+        return rangeWithDots;
+    }
+
     render(){
         var totalPages = 0;
         var paginationItems = [];
@@ -21,16 +52,44 @@ class ListingPagination extends React.Component {
         } else {
             totalPages = 0;
         }
-        var pg = parseInt(this.props.page);
         if (totalPages === 0 || totalPages === 1){
             return null;
-        } else if (totalPages === 2){
-            for (let i = 1; i <= totalPages; i++){
-                if (i === pg) {
-                    paginationItems.push(<Pagination.Item key={i} active="true">{i}</Pagination.Item>);
-                } else {
-                    paginationItems.push(<Pagination.Item key={i} onClick={() => this.onClick(i)}>{i}</Pagination.Item>);
-                }
+        }
+        var pg = parseInt(this.props.page);
+        var pgnItems = this.pagination(pg, totalPages);
+        for (let i=0; i<pgnItems.length; i++){
+
+
+            let goToPage = null;
+            if (pgnItems[i] === "..."){
+                goToPage = pgnItems[i-1];
+            } else {
+                goToPage = pgnItems[i];
+            }
+            var active = false;
+            if (pgnItems[i] === pg){
+                active = true;
+            }
+            if (active){
+                paginationItems.push(
+                <Pagination.Item
+                     key={i}
+                     active
+                     onClick={() => this.onClick(goToPage)}
+                >
+                    {pgnItems[i]}
+                </Pagination.Item>
+                );
+
+            } else {
+                paginationItems.push(
+                <Pagination.Item 
+                     key={i}
+                     onClick={() => this.onClick(goToPage)}
+                >
+                    {pgnItems[i]}
+                </Pagination.Item>
+                );
             }
         }
         return (
