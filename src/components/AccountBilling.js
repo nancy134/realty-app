f31717dd7379ecb31c7d0b91911de51c6e8d0740
Nancy Piedra <nancy_piedra@yahoo.com>
2020-12-16 17:03:26 +0000

    Account billing page

--- src/components/AccountBilling.js
+++ src/components/AccountBilling.js
@@ -1,121 +1,148 @@
 import React from 'react';
 import {
     Container,
+    Button,
+    Spinner,
     Row,
-    Jumbotron,
-    Col,
-    Form,
-    Button
+    Col
 } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { 
-    faCcVisa,
-    faCcMastercard
-} from '@fortawesome/fontawesome-free-brands';
+import DropIn from 'braintree-web-drop-in-react';
+import billingService from '../services/billing';
 
 class AccountBilling extends React.Component {
 
-render(){
+    constructor(props){
+        super(props);
+
+        this.handleNoPaymentMethodRequestable = this.handleNoPaymentMethodRequestable.bind(this);
+        this.handlePaymentMethodRequestable = this.handlePaymentMethodRequestable.bind(this);
+        this.handlePaymentOptionSelected = this.handlePaymentOptionSelected.bind(this);
+        this.handleInstance = this.handleInstance.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+
+        this.state = {
+            clientToken: null,
+            paymentMethodRequestable: false,
+            paymentSpinner: false,
+            showError: false,
+            errorMessage: ""
+        };
+    }
+
+    componentDidMount(){
+        var that = this;
+        billingService.getClientToken().then(function(result){
+            that.setState({
+                clientToken: result.clientToken
+            });
+        }).catch(function(err){
+            this.setState({
+                showError: true,
+                errorMessage: err.message
+            });
+        });
+    }
+
+    handleNoPaymentMethodRequestable(event){
+        this.setState({
+            paymentMethodRequestable: false
+        });
+    }
+
+    handlePaymentMethodRequestable(event){
+        this.setState({
+            paymentMethodRequestable: true
+        });
+    }
+
+    handlePaymentOptionSelected(event){
+    }
+
+    handleInstance(instance){
+        this.instance = instance;
+        if (this.instance.isPaymentMethodRequestable){
+            this.setState({
+                paymentMethodRequestable: true
+            });
+        }
+    }
+
+    handleUpdate(){
+        var that = this;
+        this.setState({
+            paymentSpinner: true
+        });   
+        this.instance.requestPaymentMethod().then(function(result){
+            billingService.setPaymentMethod(result.nonce).then(function(paymentResult){
+                console.log(paymentResult);
+                 that.setState({
+                     paymentSpinner: false,
+                     showError: false,
+                     errorMessage: "" 
+                 });
+            }).catch(function(err){
+                console.log(err);
+                that.setState({
+                    paymentSpinner: false,
+                    showError: true,
+                    errorMessage: err.message
+                });
+            });
+        }).catch(function(err){
+            console.log(err);
+            that.setState({
+                paymentSpinner: false,
+                showError: true,
+                errorMessage: err.message
+            });
+        });
+    }
+
+    render(){
     return(
     <React.Fragment>
-        <Container>
-            <Row className="pt-5">
+        <Container className="p-5">
+            <Row><Col xs={2}></Col><Col xs={8}>
+            { this.state.clientToken ?
+            <DropIn
+                options={{
+                    authorization: this.state.clientToken
+                }}
+                onInstance={this.handleInstance}
+                onNoPaymentMethodRequestable={this.handleNoPaymentMethodRequestable}
+                onPaymentMethodRequestable={this.handlePaymentMethodRequestable}
+                onPaymentOptionSelected={this.handlePaymentOptionSelected}
+            />
+            : null }
+            </Col><Col xs={2}></Col></Row>
+
+
+            <Row>
+                <Col xs={4}></Col>
+                <Col xs={4}>
+                   <Button
+                        onClick={this.handleUpdate}
+                        disabled={!this.state.paymentMethodRequestable}
+                        block
+                    >
+                        {this.state.paymentSpinner ?
+                        <Spinner
+                            as="span"
+                            animation="border"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                        />
+                        :
+                        <span>Update Payment Method</span>}
+                    </Button>
+                </Col>
+                <Col xs={4}></Col>
             </Row>
-            <Jumbotron className="pt-3">
-                <Row>
-                    <Col>
-                    <h1 className="text-center">Billing Information</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form>
-                           <Row>
-                               <Col sm="2"></Col>
-                               <Col sm="4">
-                                   <FontAwesomeIcon size="3x" icon={faCcVisa} />&nbsp;
-                                   <FontAwesomeIcon size="3x" icon={faCcMastercard} />
-                               </Col>
-                           </Row>
-                           <Form.Group as={Row} controlId="name">
-                               <Form.Label column sm="2">Name on Card</Form.Label>
-                               <Col sm="4">
-                                   <Form.Control type="text" />
-                               </Col>
-                           </Form.Group>
-
-                           <Form.Group as={Row} controlId="cardNumber">
-                               <Form.Label column sm="2">Card Number</Form.Label>
-                               <Col sm="4">
-                                   <Form.Control type="text" />
-                               </Col>
-                               <Form.Label column sm="1">CCV</Form.Label>
-                               <Col sm="2">
-                                   <Form.Control type="text" />
-                               </Col>
-                           </Form.Group>
-
-                           <Form.Group as={Row} controlId="expiration">
-                               <Form.Label column sm="2">Expiration</Form.Label>
-                               <Col sm="2">
-                                   <Form.Control as="select">
-                                       <option>01-January</option>
-                                       <option>02-February</option>
-                                       <option>03-March</option>
-                                   </Form.Control>
-                               </Col>
-                               <Col sm="2">
-                                   <Form.Control as="select">
-                                       <option>2020</option>
-                                       <option>2021</option>
-                                       <option>2022</option>
-                                   </Form.Control>
-                               </Col>
-                           </Form.Group>
-
-                           <Form.Group as={Row} controlId="address1">
-                               <Form.Label column sm="2">Billing Address</Form.Label>
-                               <Col sm="4">
-                                   <Form.Control type="text" />
-                               </Col>
-                           </Form.Group>
-                           <Form.Group as={Row} controlId="address2">
-                               <Form.Label column sm="2"></Form.Label>
-                               <Col sm="4">
-                                   <Form.Control type="text" />
-                               </Col>
-                           </Form.Group>
-                          <Form.Group as={Row} controlId="city">
-                               <Form.Label column sm="2">City</Form.Label>
-                               <Col sm="4">
-                                   <Form.Control type="text" />
-                               </Col>
-                           </Form.Group>
-                           <Form.Group as={Row} >
-                               <Form.Label column sm="2">State</Form.Label>
-                               <Col sm="3">
-                                   <Form.Control as="select" >
-                                       <option>State</option>
-                                       <option>Alabama</option>
-                                       <option>Alaska</option>
-                                   </Form.Control>
-                               </Col>
-                               <Form.Label column sm="1">Zip</Form.Label>
-                               <Col sm="3">
-                                   <Form.Control type="text" />
-                               </Col>
-                           </Form.Group>
-                       </Form>
-                   </Col>
-               </Row>
-               <div className="text-right">
-                   <Button variant="success">Add Card</Button>
-               </div>
-            </Jumbotron>
         </Container>
     </React.Fragment>
     );
-}
+    }
 }
 
 export default AccountBilling;
