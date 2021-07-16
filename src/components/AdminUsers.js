575a3246eed3eab00a9d7e4b12c80e188b804b84
Nancy Piedra <nancy_piedra@yahoo.com>
2021-07-16 12:46:38 +0000

    Admin Users

--- src/components/AdminUsers.js
+++ src/components/AdminUsers.js
@@ -48,12 +48,92 @@ class AdminUsers extends React.Component {
                 <MaterialTable
                     icons={tableIcons}
                     columns={[
-                        { title: 'id', field: 'id'},
-                        { title: 'email', field: 'email'},
-                        { title: 'cognitoId', field: 'cognitoId'},
-                        { title: 'first', field: 'first'},
-                        { title: 'last', field: 'last'}
+                        
+                        { title: 'ID', field: 'id' },
+                        { title: 'Email', field: 'email'},
+                        { title: 'CognitoId', field: 'cognitoId'},
+                        { title: 'First', field: 'first'},
+                        { title: 'Last', field: 'last'}
                     ]}
+                    options={{
+                      }}
+                    detailPanel={rowData => {
+                        return (
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <table class="table table-striped">
+                <tr>
+                    <th>Association</th>
+                    <th>Association Status</th>
+                    <th>Association Token</th>
+                </tr>
+                <tr>
+                    <td>{rowData.AssociationId}</td>
+                    <td>{rowData.associationStatus}</td>
+                    <td>{rowData.associationToken}</td>
+                </tr>
+                <tr>
+                    <th>Title</th>
+                    <th colspan="2">Company</th>
+                </tr>
+                <tr>
+                    <td>{rowData.title}</td>
+                    <td colspan="2">{rowData.company}</td>
+                </tr>
+
+                <tr>
+                    <th colspan="3">Address</th>
+                </tr>
+
+                <tr>
+                    <td colspan="3">{rowData.address1} {rowData.address1}<br />{rowData.city}, {rowData.state} {rowData.zip}</td>
+                </tr>
+            </table>
+        </div>
+        <div class="col">
+            <table class="table table-striped">
+                <tr>
+                    <th>Website</th>
+                    <th>Phone (Mobile)</th>
+                    <th>Phone (Office)</th>
+                </tr>
+                <tr>
+                    <td>{rowData.website}</td>
+                    <td>{rowData.mobilePhone}</td>
+                    <td>{rowData.officePhone}</td>
+                </tr>
+                <tr>
+                    <th colspan="3">Bio</th>
+                </tr>
+
+                <tr>
+                    <td colspan="3">{rowData.bio}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <strong>Role:</strong> {rowData.role}
+        </div>
+        <div class="col">
+            <strong>Account Created:</strong> {rowData.createdAt}
+        </div>
+        <div class="col">
+            <strong>Account Updated:</strong> {rowData.updatedAt}
+        </div>
+    </div>
+</div>
+                              
+                        )
+                      }}
+
+                      onSelectionChange={rows => {
+                        alert("Selected id is:  " + rows[0].id);
+                      }}
+
                     data={query =>
                         new Promise((resolve, reject) => {
                             var page = query.page + 1;
@@ -72,8 +152,8 @@ class AdminUsers extends React.Component {
                             });
                         })
                     }
-                        
                     title="Users"
+                    onRowClick={(event, rowData, togglePanel) => togglePanel()}
                 />  
             </div>
         )
