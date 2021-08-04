b5d32cddbef2f65e41ce4f4c962f61315f816ddf
Nancy Piedra <nancy_piedra@yahoo.com>
2021-08-04 19:31:10 +0000

    Admin Listings

--- src/components/AdminListings.js
+++ src/components/AdminListings.js
@@ -57,6 +57,162 @@ class AdminListings extends React.Component {
                         { title: 'city', field: 'city'},
                         { title: 'state', field: 'state'}
                     ]}
+
+
+                    detailPanel={rowData => {
+                        return (
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <strong>Listing ID:</strong> {rowData.ListingId}
+                                </div>
+                                <div class="col">
+                                    <strong>Status:</strong> {rowData.publishStatus}
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <table class="table table-striped">
+                                        <tr>
+                                            <th colspan="3">Address</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="3">{rowData.address}<br />{rowData.city}, {rowData.state} {rowData.zip}</td>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="3">Landlord Expenses</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="3">{rowData.landlordExpenses}</td>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="3">Tenant Expenses</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="3">{rowData.tenantExpenses}</td>
+                                        </tr>       
+                                        <tr>
+                                            <th colspan="3">Short Description</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="3">{rowData.shortDescription}</td>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="3">Long Description</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="3">{rowData.longDescription}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Listing Type</th>
+                                            <th>Listing Price</th>
+                                            <th>Property Type/s</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.listingType}</td>
+                                            <td>{rowData.listingPrice}</td>
+                                            <td>{rowData.propertyType}/{rowData.propertyTypes}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Total Building Size/Total Available Space</th>
+                                            <th>Lot Size</th>
+                                            <th>Taxes</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.totalBuildingSize}/{rowData.totalAvailableSpace}</td>
+                                            <td>{rowData.lotSize}</td>
+                                            <td>{rowData.taxes}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Parking</th>
+                                            <th>Floors</th>
+                                            <th># Of Units</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.parking}</td>
+                                            <td>{rowData.floors}</td>
+                                            <td>{rowData.totalNumberOfUnits}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Building Class</th>
+                                            <th>Zone</th>
+                                            <th>Year Build</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.buildingClass}</td>
+                                            <td>{rowData.zone}</td>
+                                            <td>{rowData.yearBuilt}</td>
+                                        </tr>     
+                                        <tr>
+                                            <th>Ceiling Height</th>
+                                            <th>Drive-in Doors</th>
+                                            <th>Loading Docks</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.ceilingHeight}</td>
+                                            <td>{rowData.driveInDoors}</td>
+                                            <td>{rowData.loadingDocks}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Nets</th>
+                                            <th>Gross Income</th>
+                                            <th>Net Income</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.nets}</td>
+                                            <td>{rowData.grossIncome}</td>
+                                            <td>{rowData.netIncome}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>Cap Rate</th>
+                                            <th>Price per Sq. Foot</th>
+                                            <th>Maintenance</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.capRate}</td>
+                                            <td>{rowData.pricePerFoot}</td>
+                                            <td>{rowData.maintenance}</td>
+                                        </tr>
+                                        <tr>
+                                            <th>HVAC</th>
+                                            <th>Security</th>
+                                            <th>HOA Feets</th>
+                                        </tr>
+                                        <tr>
+                                            <td>{rowData.hvac}</td>
+                                            <td>{rowData.seciroty}</td>
+                                            <td>{rowData.hoaFees}</td>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="3">Utilities</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="3">{rowData.utilities}</td>
+                                        </tr>
+                                        <tr>
+                                            <th colspan="3">Amenities</th>
+                                        </tr>
+                                        <tr>
+                                            <td colspan="3">{rowData.amenities}</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <strong>Owner:</strong> {rowData.owner}
+                                </div>       
+                                <div class="col">
+                                    <strong>Created:</strong> {rowData.createdAt}
+                                </div>
+                                <div class="col">
+                                    <strong>Updated:</strong> {rowData.updatedAt}
+                                </div>
+                            </div>
+                        </div>
+                        )
+                    }}
+
                     data={query =>
                         new Promise((resolve, reject) => {
                             var page = query.page + 1;
